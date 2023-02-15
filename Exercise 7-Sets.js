e8a3943587ffda82e5f3c348cb80fc77f8ee8726
alexanderrami <124542430+alexanderrami@users.noreply.github.com>
2023-02-15 11:37:06 -0500

    Add files via upload

---
+++ Exercise 7-Sets.js
@@ -0,0 +1,11 @@
+const miFamilia = new Set(["merardo", "gabriel", "juver", "ofelia","edgar"]);
+const miSet = new Set(miFamilia)
+
+console.log(miFamilia)
+console.log(miSet)
+
+
+miSet.add("Alex")
+console.log(miSet)
+miSet.add("Javascript")
+console.log(miSet)
